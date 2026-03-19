97def1095128124ad77e1718de561f7238aa62ed
berdi00 <berdinazarov731@gmail.com>
2026-03-19 16:48:15 +0500

    build: change Vite base path to relative for static deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
